466e00573f976b6dbdf0cb5ab42c1765831b682d
khaled39ab <khaled39ab@gmail.com>
2022-04-15 18:05:55 +0600

    Display countries details

--- JS/app.js
+++ JS/app.js
@@ -22,3 +22,18 @@ const displayCountries = (countries) => {
     })
 }
 
+const countryDetails = (name) =>{
+    fetch(`https://restcountries.com/v2/name/${name}`)
+    .then (res => res.json())
+    .then (data => showDetails(data[0]))
+}
+const showDetails = (details) =>{
+    console.log(details);
+    const countryInfo = document.getElementById("detailsI-Info")
+    const div = document.createElement('div')
+    div.innerHTML = `
+        <h2>Name: ${details.name}</h2>
+
+    `
+    countryInfo.appendChild(div)
+}
